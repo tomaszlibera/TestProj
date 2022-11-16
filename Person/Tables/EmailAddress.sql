20c8c57cb4424ca1f776134bc67f864f63b00a37
tomaszlibera <tomasz.libera@datacommunity.pl>
2022-11-16 19:43:48 +0100

    kolejna zmiana

--- Person/Tables/EmailAddress.sql
+++ Person/Tables/EmailAddress.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [Person].[EmailAddress] (
     [BusinessEntityID] INT              NOT NULL,
     [EmailAddressID]   INT              IDENTITY (1, 1) NOT NULL,
-    [EmailAddress]     NVARCHAR (50)    NULL,
+    [EmailAddress]     NVARCHAR (60)    NULL,
     [rowguid]          UNIQUEIDENTIFIER ROWGUIDCOL NOT NULL,
     [ModifiedDate]     DATETIME         NOT NULL
 );
