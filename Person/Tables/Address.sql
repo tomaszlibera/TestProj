8153b08d9dd13fc55871888d2e523284670d0a21
tomaszlibera <tomasz.libera@datacommunity.pl>
2022-11-16 19:54:18 +0100

    zmiana tyou danych

--- Person/Tables/Address.sql
+++ Person/Tables/Address.sql
@@ -2,12 +2,13 @@ CREATE TABLE [Person].[Address] (
     [AddressID]       INT               IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL,
     [AddressLine1]    NVARCHAR (60)     NOT NULL,
     [AddressLine2]    NVARCHAR (60)     NULL,
-    [City]            NVARCHAR (30)     NOT NULL,
+    [City]            NVARCHAR (40)     NOT NULL,
     [StateProvinceID] INT               NOT NULL,
     [PostalCode]      NVARCHAR (15)     NOT NULL,
     [SpatialLocation] [sys].[geography] NULL,
     [rowguid]         UNIQUEIDENTIFIER  ROWGUIDCOL NOT NULL,
-    [ModifiedDate]    DATETIME          NOT NULL
+    [ModifiedDate]    DATETIME          NOT NULL,
+    ColY int
 );
 GO
 
